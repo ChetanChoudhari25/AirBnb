06a47d9bb921561912d44ce96ee4ebc8de05f702
ChetanChoudhari25 <cchoudhari205@gmail.com>
2025-02-13 21:05:55 +0530

    path correction2

--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 const Listing = require("./models/listing.js");
 const { listingSchema, reviewSchema } = require("./schema.js");
 const ExpressError = require("./utils/ExpressError.js");
-const Review = require("../AirBnb/models/review.js");
+const Review = require("./models/review.js");
 
 
 module.exports.isLoggedIn = (req, res, next)=>{
